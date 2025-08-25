5f4b5b48a9e17e23d1e61f03d15f43e86e9b823c
Lokatian <lovromarinc123@gmail.com>
2025-08-25 07:33:46 +0200

    added basics of  grid

--- ExcelExportWebApp/EEWA.WEB/Components/Pages/Import/ImportViewModel.cs
+++ ExcelExportWebApp/EEWA.WEB/Components/Pages/Import/ImportViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using EEWA.DAL.Context;
 using EEWA.DAL.Model;
+using Telerik.Blazor.Components;
 namespace EEWA.WEB.Components.Pages.Import;
 
 public class ImportViewModel
@@ -27,5 +28,59 @@ public class ImportViewModel
         components.AddRange(_sandBoxContext.Components);
         projectSections.AddRange(_sandBoxContext.Sections);
         var firstSection = projectSections.Where(s => s.SectionID == 1).FirstOrDefault();
+    }
+
+    public async Task OnGridCreate(GridCommandEventArgs args)
+    {
+
+        //var transObject = args.Item as ModelBase;
+        //try
+        //{
+        //    await AddItemAsync(transObject);
+        //}
+        //catch (Exception e) { }
+
+
+        //ClearAndAddToCollections(transObject);
+    }
+    public async Task DeleteComponentAsync(Component component, string? secitonID)
+    {
+
+    }
+    public async Task OnGridUpdate(GridCommandEventArgs args)
+    {
+        try
+        {
+            //var transObject = args.Item as ModelBase;
+            //var ctxItem = IndigomatContext.Find(transObject.GetType(), transObject.Id);
+            //IndigomatContext.Entry(ctxItem).CurrentValues.SetValues(transObject);
+            //var res = IndigomatContext.SaveChanges();
+        }
+        catch (Exception ex)
+        {
+            throw;
+        }
+    }
+    public async Task OnGridDelete(GridCommandEventArgs args)
+    {
+
+        //var item = args.Item as ModelBase;
+        //await DeleteItemAsync(item.Id, item);
+    }
+    public async Task AddItemAsync(ProjectSection section)
+    {
+        //IndigomatContext.Add(section);
+        //await IndigomatContext.SaveChangesAsync();
+        //ClearAndAddToCollections(section);
+    }
+    public async Task DeleteItemAsync(int id, Component item)
+    {
+        //var ctxItem = IndigomatContext.Find(item.GetType(), item.Id);
+        //if (ctxItem != null)
+        //{
+        //    IndigomatContext.Remove(ctxItem);
+        //    await IndigomatContext.SaveChangesAsync();
+        //    ClearAndAddToCollections(item);
+        //}
     }
 }
